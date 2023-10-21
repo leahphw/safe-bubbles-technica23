9a62f4b3a17b4fcb3d70dadff2ca9032342f4b58
leahphw <leah.phw@gmail.com>
2023-10-21 15:50:32 -0400

    set up google maps api

--- time-lot/src/App.js
+++ time-lot/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+const center = {
+  lat: 37.7749,
+  lng: -122.4194,
+};
 
 function App() {
+  const [userPosition, setUserPosition] = useState(null);
+
+  useEffect(() => {
+    // Get the user's current location using the Geolocation API
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <LoadScript googleMapsApiKey="YOUR_API_KEY">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={userPosition || center}
+          zoom={10}
         >
-          Learn React
-        </a>
-      </header>
+          {userPosition && <Marker position={userPosition} />}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 }
